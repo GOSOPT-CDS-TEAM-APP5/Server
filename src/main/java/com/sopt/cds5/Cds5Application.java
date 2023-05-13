659e820fe0fe4c4d01d15777ad03b3455cb61839
hayoun <fishingest@kookmin.ac.kr>
2023-05-13 16:34:05 +0900

    [init] 프로젝트 기초 세팅

---
+++ src/main/java/com/sopt/cds5/Cds5Application.java
@@ -0,0 +1,13 @@
+package com.sopt.cds5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cds5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cds5Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/sopt/cds5/Cds5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sopt.cds5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cds5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
