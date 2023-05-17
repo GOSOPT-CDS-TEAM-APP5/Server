10a035424d8351eb33cb48de78a0d942634d746e
hayoun <fishingest@kookmin.ac.kr>
2023-05-18 01:47:57 +0900

    [feat] Schedule 엔티티 구현

---
+++ src/main/java/com/sopt/cds5/domain/Schedule.java
@@ -0,0 +1,36 @@
+package com.sopt.cds5.domain;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Schedule {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="schedule_id")
+    private Long id;
+
+    @ManyToOne(fetch=FetchType.LAZY)
+    @JoinColumn(name = "movie_id")
+    private Movie movie;
+
+    @ManyToOne(fetch=FetchType.LAZY)
+    @JoinColumn(name = "theater_id")
+    private Theater theater;
+
+    private Long multiplex;
+    private String startTime;
+    private String endTime;
+    private String currentPeople;
+    private String maxPeople;
+    private Date date;
+
+
+}
