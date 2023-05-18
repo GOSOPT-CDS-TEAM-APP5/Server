0f9924c5a39308583a15247424062d1a582c1f7e
hayoun <fishingest@kookmin.ac.kr>
2023-05-18 17:40:09 +0900

    [fix] ScheduleRepository Multiplex 방식으로 수정

---
+++ src/main/java/com/sopt/cds5/infrastructure/MultiplexRepository.java
@@ -0,0 +1,2 @@
+package com.sopt.cds5.infrastructure;public class MultiplexRepository {
+}
--- src/main/java/com/sopt/cds5/service/ScheduleService.java
+++ src/main/java/com/sopt/cds5/service/ScheduleService.java
@@ -1,11 +1,15 @@
 package com.sopt.cds5.service;
 
+import com.sopt.cds5.controller.dto.request.ScheduleRequestDto;
+import com.sopt.cds5.controller.dto.response.MultiplexResponseDto;
 import com.sopt.cds5.controller.dto.response.ScheduleResponseDto;
 import com.sopt.cds5.controller.dto.response.TheaterResponseDto;
 import com.sopt.cds5.domain.Movie;
+import com.sopt.cds5.domain.Multiplex;
 import com.sopt.cds5.domain.Schedule;
 import com.sopt.cds5.domain.Theater;
 import com.sopt.cds5.infrastructure.MovieRepository;
+import com.sopt.cds5.infrastructure.MultiplexRepository;
 import com.sopt.cds5.infrastructure.ScheduleRepository;
 import com.sopt.cds5.infrastructure.TheaterRepository;
 import lombok.RequiredArgsConstructor;
@@ -23,20 +27,29 @@ public class ScheduleService {
     private final ScheduleRepository scheduleRepository;
     private final TheaterRepository theaterRepository;
     private final MovieRepository movieRepository;
+    private final MultiplexRepository multiplexRepository;
     public List<TheaterResponseDto> getSchedule(LocalDate date,Long movieId,List<Long> TheaterIdList){
         List<TheaterResponseDto> scheduleList=new ArrayList<>();
         Optional<Movie> movie= movieRepository.findByMovieId(movieId);
         for (Long TheaterId:TheaterIdList
              ) {
             Theater theater=theaterRepository.findById(TheaterId);
-            List<Schedule> schedules =scheduleRepository.findByTheaterAndDateAndMovie(theater,date,movie.get());
-            List<ScheduleResponseDto> scheduleResponseDtoList=new ArrayList<>();
-            for (Schedule schedule:schedules
+            List<Multiplex> multiplexList=multiplexRepository.findAllByTheater(theater);
+            List<MultiplexResponseDto> multiplexResponseDtoList=new ArrayList<>();
+            for (Multiplex multiplex:multiplexList
                  ) {
-                scheduleResponseDtoList.add(ScheduleResponseDto.of(schedule.getMovieType(),schedule.getMultiplex(),schedule.getStartTime(),schedule.getEndTime(),schedule.getCurrentPeople(),schedule.getCurrentPeople()));
-
+                List<Schedule> schedules=scheduleRepository.findByTheaterAndDateAndMovieAndMultiplex(theater,date,movie.get(),multiplex);
+                List<ScheduleResponseDto> scheduleResponseDtoList=new ArrayList<>();
+                for (Schedule schedule:schedules
+                     ) {
+                    scheduleResponseDtoList.add(ScheduleResponseDto.of(schedule.getMovieType(),schedule.getStartTime(),schedule.getEndTime(),schedule.getCurrentPeople(),schedule.getMaxPeople()));
+                }
+                multiplexResponseDtoList.add(MultiplexResponseDto.of(multiplex.getMultiplexType(),multiplex.getMultiplexName(),scheduleResponseDtoList));
             }
-            scheduleList.add(TheaterResponseDto.of(theater.getTheaterName(),scheduleResponseDtoList));
+
+
+            scheduleList.add(TheaterResponseDto.of(theater.getTheaterName(),multiplexResponseDtoList));
+
         }
         return scheduleList;
 
