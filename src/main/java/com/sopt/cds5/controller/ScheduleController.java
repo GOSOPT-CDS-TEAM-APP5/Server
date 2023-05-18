95994c5550f4004b938a4e0b054d1e7d9873ac5b
hayoun <fishingest@kookmin.ac.kr>
2023-05-18 20:47:16 +0900

    [feat] Params 형식의 통신

--- src/main/java/com/sopt/cds5/controller/ScheduleController.java
+++ src/main/java/com/sopt/cds5/controller/ScheduleController.java
@@ -7,11 +7,14 @@ import com.sopt.cds5.domain.Schedule;
 import com.sopt.cds5.exception.SuccessStatus;
 import com.sopt.cds5.service.ScheduleService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @RestController
@@ -21,8 +24,8 @@ public class ScheduleController {
     private final ScheduleService scheduleService;
 
     @GetMapping("/schedule")
-    public ApiResponseDto<List<TheaterResponseDto>> getSchedule(@RequestBody @Validated ScheduleRequestDto scheduleRequestDto){
-        List<TheaterResponseDto> theaterResponseDtoList=scheduleService.getSchedule(scheduleRequestDto.getDate(),scheduleRequestDto.getMovieId(),scheduleRequestDto.getTheaterId());
+    public ApiResponseDto<List<TheaterResponseDto>> getSchedule(@DateTimeFormat(pattern = "yyyy-MM-dd") @RequestParam  final LocalDate date, @RequestParam final Long movieId, @RequestParam final List<Long> theaterId){
+        List<TheaterResponseDto> theaterResponseDtoList=scheduleService.getSchedule(date,movieId,theaterId);
         return ApiResponseDto.success(SuccessStatus.SEARCH_SUCCESS,theaterResponseDtoList);
     }
 }
